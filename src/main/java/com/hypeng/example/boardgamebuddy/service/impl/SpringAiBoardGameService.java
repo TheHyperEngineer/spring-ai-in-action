811dbda0f16a19d6ce3d534f6728e539866d773f
Hyper Engineer <admin-user@hyper-engineer.com>
2026-02-13 21:04:31 -0600

    Prompt Template with context

--- src/main/java/com/hypeng/example/boardgamebuddy/service/impl/SpringAiBoardGameService.java
+++ src/main/java/com/hypeng/example/boardgamebuddy/service/impl/SpringAiBoardGameService.java
@@ -4,23 +4,37 @@ import com.hypeng.example.boardgamebuddy.dto.Answer;
 import com.hypeng.example.boardgamebuddy.dto.Question;
 import com.hypeng.example.boardgamebuddy.service.BoardGameService;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SpringAiBoardGameService implements BoardGameService {
 
     private final ChatClient chatClient;
+    private final GameRulesService gameRulesService;
 
-    public SpringAiBoardGameService(ChatClient.Builder chatClientBuilder) {
+    @Value("classpath:/promptTemplates/questionPromptTemplate.st")
+    private Resource questionPromptTemplate;
+
+    public SpringAiBoardGameService(ChatClient.Builder chatClientBuilder,
+                                    GameRulesService gameRulesService) {
         this.chatClient = chatClientBuilder.build();
+        this.gameRulesService = gameRulesService;
     }
 
     @Override
     public Answer askQuestion(Question question) {
-        String prompt = "Answer this question about " +question.gameTitle() + ": " + question.question();
+        var gameRules = gameRulesService.getRulesFor(question.gameTitle());
 
-        String answerText = chatClient.prompt()
-                .user(prompt)
+        var answerText = chatClient.prompt()
+                .user(userSpec -> userSpec
+                        .text(questionPromptTemplate)
+                        .param("gameTitle", question.gameTitle())
+                        .param("question", question.question())
+                        .param("rules", gameRules))
+                .advisors(new SimpleLoggerAdvisor())
                 .call()
                 .content();
 
