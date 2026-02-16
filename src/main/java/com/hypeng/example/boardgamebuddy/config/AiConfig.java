7b47bf29c1bf27d6559a69f88c74a49cbebcfbb1
Hyper Engineer <admin-user@hyper-engineer.com>
2026-02-16 12:06:56 -0600

    Adding ChatMemory with VectorStore

--- src/main/java/com/hypeng/example/boardgamebuddy/config/AiConfig.java
+++ src/main/java/com/hypeng/example/boardgamebuddy/config/AiConfig.java
@@ -1,50 +1,22 @@
 package com.hypeng.example.boardgamebuddy.config;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.advisor.PromptChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
-import org.springframework.ai.chat.memory.ChatMemory;
-import org.springframework.ai.chat.memory.ChatMemoryRepository;
-import org.springframework.ai.chat.memory.MessageWindowChatMemory;
-import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
-import org.springframework.ai.chat.memory.repository.jdbc.PostgresChatMemoryRepositoryDialect;
+import org.springframework.ai.chat.client.advisor.vectorstore.VectorStoreChatMemoryAdvisor;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-
 @Configuration
 public class AiConfig {
 
     @Bean
-    public ChatMemoryRepository chatMemoryRepository(DataSource dataSource) {
-        return JdbcChatMemoryRepository
-                .builder()
-                .dialect(new PostgresChatMemoryRepositoryDialect())
-                .dataSource(dataSource)
-                .build();
-    }
-
-        @Bean
-    public ChatMemory chatMemory(ChatMemoryRepository chatMemoryRepository) {
-        return MessageWindowChatMemory
-                .builder()
-                .chatMemoryRepository(chatMemoryRepository)
-                .maxMessages(10)
-                .build();
-    }
-
-    @Bean
-    ChatClient chatClient(
-            ChatClient.Builder chatClientBuilder,
-            VectorStore vectorStore,
-            ChatMemory chatMemory) {
+    ChatClient chatClient(ChatClient.Builder chatClientBuilder, VectorStore vectorStore) {
         return chatClientBuilder
                 .defaultAdvisors(
-                        PromptChatMemoryAdvisor
-                                .builder(chatMemory)
+                        VectorStoreChatMemoryAdvisor
+                                .builder(vectorStore)
                                 .build(),
                         QuestionAnswerAdvisor
                                 .builder(vectorStore)
