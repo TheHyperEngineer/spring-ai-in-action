078fb8c898cb00e4535cd43750282d22973541d7
Hyper Engineer <admin-user@hyper-engineer.com>
2026-02-16 11:54:24 -0600

    Adding ChatMemory with PostgrSql

--- src/main/java/com/hypeng/example/boardgamebuddy/config/AiConfig.java
+++ src/main/java/com/hypeng/example/boardgamebuddy/config/AiConfig.java
@@ -6,20 +6,33 @@ import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvi
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.ChatMemoryRepository;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
+import org.springframework.ai.chat.memory.repository.jdbc.PostgresChatMemoryRepositoryDialect;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class AiConfig {
 
     @Bean
+    public ChatMemoryRepository chatMemoryRepository(DataSource dataSource) {
+        return JdbcChatMemoryRepository
+                .builder()
+                .dialect(new PostgresChatMemoryRepositoryDialect())
+                .dataSource(dataSource)
+                .build();
+    }
+
+        @Bean
     public ChatMemory chatMemory(ChatMemoryRepository chatMemoryRepository) {
         return MessageWindowChatMemory
                 .builder()
                 .chatMemoryRepository(chatMemoryRepository)
-                .maxMessages(50)
+                .maxMessages(10)
                 .build();
     }
 
